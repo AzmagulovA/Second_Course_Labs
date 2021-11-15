d0e7ef7e6c926a6e28c2fe977eae1e83e9f8e4b4
AzmagulovA <74853801+AzmagulovA@users.noreply.github.com>
2021-11-15 18:57:23 +0300

    Add files via upload

---
+++ Лабы с# 22.10.21/Лабораторная работа 9/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа__9
+{
+    class Program
+    {
+        static Time plus_sec(Time b, int a)
+        {
+            return new Time 
+            {
+                Seconds = (a + b.Seconds) % 60,
+                Minutes = (b.Minutes + (a + b.Seconds) / 60) % 60,
+                Hours = b.Hours + (b.Minutes + (a + b.Seconds) / 60) / 60,
+            };
+        }
+        
+        static void Main(string[] args)
+        {
+            Time res=new Time();
+            Console.WriteLine(res.Minutes);
+            Console.WriteLine(res.bool_q());
+            res = new Time(5,59,59);
+            
+
+           res.print();
+            int s;
+            bool ok;
+            do
+            {
+                Console.WriteLine("sec?");
+                ok = int.TryParse(Console.ReadLine(), out s);
+            } while (!ok);
+            Console.WriteLine("Результат добавления секунд к типу Time");     
+             res = res.pl_sec(s);
+            res.print();
+            Console.WriteLine("Повторное прибавление введенного числа. Результат:");
+            res=plus_sec(res, s);
+            res.print();
+            ++res;
+            res++;
+            res.print();
+            --res;
+            res--;
+            --res;
+            res.print();
+            Console.WriteLine($"Количество созданных экзепляров класса: ");
+            res.print_count();
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
+
