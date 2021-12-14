93b9b3d4a15107843df2edc01c430217c11fab7c
AzmagulovA <74853801+AzmagulovA@users.noreply.github.com>
2021-12-14 23:45:49 +0500

    Add files via upload

---
+++ Лабы с# 22.10.21/lab10/IExecutable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal interface IExecutable
+    {
+
+        void print();
+        
+        // public IExecutable[] views { get; set; }
+    }
+}
