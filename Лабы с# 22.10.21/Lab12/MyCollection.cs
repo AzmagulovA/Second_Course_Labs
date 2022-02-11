30e8d36b55221d2591603847aeb58c2fbf33ff55
AzmagulovA <74853801+AzmagulovA@users.noreply.github.com>
2022-02-11 23:30:08 +0500

    Add files via upload

---
+++ Лабы с# 22.10.21/Lab12/MyCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+     class MyCollection<T>:IEnumerable<T>
+    {
+
+
+        class MyNumerator<T> : IEnumerator<T>
+        {
+            Point<T> beg;
+            Point<T> current;
+            public MyNumerator(MyCollection<T> list)
+            {
+                beg = list.beg;
+                current = null;
+            }
+            public T Current => current.data;
+            object IEnumerator.Current => current;
+            public void Dispose()
+            {
+
+            }
+            public bool MoveNext()
+            {
+                if (current == null)
+                {
+                    current = beg;
+                }
+                else
+                    current = current.next;
+                return current != null;
+            }
+
+        }
+        Point<T> beg = null;
+
+
+    }
+}
